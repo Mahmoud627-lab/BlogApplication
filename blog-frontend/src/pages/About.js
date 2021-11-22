15aa02999a6ed061278ce6a203275a1d28fc2746
Mahmoud627-lab <79513795+Mahmoud627-lab@users.noreply.github.com>
2021-11-22 22:31:46 +0100

    second commit

---
+++ blog-frontend/src/pages/About.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const About = () => {
+    return(
+        <div className="mb-20">
+            <h1 className="sm:text-4xl text -2xl font-bold mt-6 mb-6 text-gray-900">About</h1>
+            <p className="mx-auto leading-relaxed text-base mb-4">
+               Cela faisait longtemps que plusieurs historiens l’espéraient alors que d’autres groupements d’intérêts
+                diffusaient depuis des décennies des informations au sein des Chambres fédérales
+            </p>
+
+            <p className="mx-auto leading-relaxed text-base mb-4">
+              A 19 ans, LinkedIn est le grand-père des réseaux sociaux.
+              Créé en 2002, soit deux ans avant Facebook (4 ans avant Twitter, 8 ans avant Instagram et 14 ans avant TikTok)
+            </p>
+
+            <p className="mx-auto leading-relaxed text-base mb-4">
+            Lorem Ipsum est un générateur de faux textes aléatoires. Vous choisissez le nombre de paragraphes, de mots ou de listes. Vous obtenez alors un texte aléatoire que vous pourrez ensuite utiliser librement dans vos maquettes.
+            Le texte généré est du pseudo latin et peut donner l'impression d'être du vrai texte.
+            Faux-Texte est une réalisation du studio de création de sites internet indépendant Prélude Prod.
+            Si vous aimez la photographie d'art et l'esprit zen, jetez un œil sur le site de ce photographe à Palaiseau, en Essonne (France)
+            </p>
+        </div>
+    );
+}
+
+export default About;
